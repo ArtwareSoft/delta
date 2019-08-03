326ad99bd2f170a9f3be26f28dac1da599cf0f0b
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-08-03 09:37:35 +0300

    mapValue checks if result was changed

--- src/core-delta-components.js
+++ src/core-delta-components.js
@@ -75,13 +75,46 @@ jb.component('inc.mapValues', {
     type: 'incremental',
     impl: { $: 'inc.delta-without-cache', 
         update: (ctx,{transformationFunc}) => {
-            const mapProfile = transformationFunc.profile.$mapValues || transformationFunc.profile.map
-            return jb.objFromEntries(jb.entries(ctx.data).map(e=>e[0] == '$orig' ? e : [e[0], ctx.setData(e[1]).run(mapProfile) ]))
+            const mapProfile = jb.propOfProfile(transformationFunc.profile,'map')
+//            return jb.objFromEntries(jb.entries(ctx.data).map(e=>e[0] == '$orig' ? e : [e[0], ctx.setData(e[1]).run(mapProfile) ]))
+
+            const calcultedEntries = jb.entries(ctx.data).filter(e=>e[0] != '$orig')
+                .map(e=>({ prop: e[0], orig: ctx.setData(ctx.data.$orig[e[0]]).run(mapProfile), newVal: ctx.setData(e[1]).run(mapProfile)}))
+                .filter(e=>e.newVal != e.orig)
+            
+            const newOrigEntry = jb.objFromEntries(calcultedEntries.map(({prop,orig}) =>[prop, orig]))
+            return jb.objFromEntries(calcultedEntries.map(({prop,newVal}) =>[prop, newVal])
+                .concat([['$orig', newOrigEntry]]))
         },
         splice: (ctx,{transformationFunc}) => {
             const delta = ctx.data
-            const mapProfile = transformationFunc.profile.$mapValues || transformationFunc.profile.map
+            const mapProfile = jb.propOfProfile(transformationFunc.profile,'map')
             return Object.assign({}, delta.$splice, { toAdd: delta.$splice.toAdd.map(v=>ctx.setData(v).run(mapProfile)) })
+        },
+    }
+})
+
+jb.component('filter', {
+    params: [
+        {id: 'exp', dynamic: true}
+    ],
+    impl: (ctx,exp) => Array.isArray(ctx.data)
+        ? ctx.data.filter(item=>exp(ctx.setData(item)))
+        : jb.objFromEntries(jb.entries(ctx.data).filter(e=>exp(ctx.setData(e[1]))))
+})
+
+jb.component('inc.filter', {
+    derivationOf: 'filter',
+    type: 'incremental',
+    impl: { $: 'inc.delta-without-cache', 
+        update: (ctx,{transformationFunc}) => {
+            const expProfile = jb.propOfProfile(transformationFunc.profile,'exp')
+            return jb.objFromEntries(jb.entries(ctx.data).map(e=>e[0] == '$orig' ? e : [e[0], ctx.setData(e[1]).run(expProfile) ]))
+        },
+        splice: (ctx,{transformationFunc}) => {
+            const delta = ctx.data
+            const expProfile = jb.propOfProfile(transformationFunc.profile,'exp')
+            return Object.assign({}, delta.$splice, { toAdd: delta.$splice.toAdd.map(v=>ctx.setData(v).run(expProfile)) })
         },
     }
 })
